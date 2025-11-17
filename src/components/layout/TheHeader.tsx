d2e007ed37763426229d1302234270f66feb5788
gabriela-restani <github@midorirestani.com>
2025-11-16 21:10:59 -0300

    feat(layout): add initial TheHeader component

---
+++ src/components/layout/TheHeader.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HandsRankingModal } from '@/components/modals/HandsRankingModal';
+export function TheHeader() {
+  return (
+    <header 
+      className="flex flex-row items-center justify-end 
+        w-full bg-neutral-300 text-neutral-900"
+    >
+      <button 
+        popoverTarget="hands-ranking-dialog"
+      >
+        Hand Ranking
+      </button>
+      <HandsRankingModal id="hands-ranking-dialog"/>
+    </header>
+  );
+}
---
+++ src/components/modals/HandsRankingModal.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { UiModal } from "@/components/ui/ui-modal";
+import type { HandsRankingModalProps } from "@/types/modals";
+
+export function HandsRankingModal({ id }: HandsRankingModalProps) {
+  return (
+    <UiModal 
+      id={id}
+    >
+      <h2>Hand Rankings</h2>
+      <ol>
+        <li>Royal Flush</li>
+        <li>Straight Flush</li>
+        <li>Four of a Kind</li>
+        <li>Full House</li>
+        <li>Flush</li>
+        <li>Straight</li>
+        <li>Three of a Kind</li>
+        <li>Two Pair</li>
+        <li>One Pair</li>
+        <li>High Card</li>
+      </ol>
+    </UiModal>
+  );
+}
