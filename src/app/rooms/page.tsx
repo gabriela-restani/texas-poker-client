a507ad7642944a29fbf0b7f82c37575934709687
gabriela-restani <github@midorirestani.com>
2025-11-23 17:40:54 -0300

    refact(RoomList): create add room modal content

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { RoomsList } from '@/components/room/RoomsList';
+import { CreateRoomForm } from '@/components/room/CreateRoomForm';
 
 export default function RoomsPage() {
   const router = useRouter();
@@ -16,11 +18,19 @@ export default function RoomsPage() {
   }, [router]);
 
   return (
-    <main className="bg-neutral-100 w-full min-h-svh text-neutral-900 flex flex-col justify-center items-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4">Salas de Poker</h1>
-        <p className="text-gray-600">Lista de salas disponíveis aparecerá aqui.</p>
-        <button className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">Criar Nova Sala</button>
+    <main className="bg-neutral-100 w-full min-h-svh text-neutral-900 flex flex-col justify-center items-center p-8">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6">Salas de Poker</h1>
+        <button 
+          className="mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
+          popoverTarget='create-room-modal'
+        >
+          Criar Nova Sala
+        </button>
+        <CreateRoomForm 
+          id="create-room-modal"
+        />
+        <RoomsList />    
       </div>
     </main>
   );
